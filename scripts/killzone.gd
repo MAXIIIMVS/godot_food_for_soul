479f4d6209fe149f68623472aafe5a3d4f9742b5
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2025-09-15 16:55:46 +0330

    Slow down the time scale and remove the collision from the player when he hits a killzone.

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,9 +1,12 @@
 extends Area2D
 
 
-func _on_body_entered(_body: Node2D) -> void:
+func _on_body_entered(body: Node2D) -> void:
+	Engine.time_scale = 0.5  # slow down
+	body.get_node("CollisionShape2D").queue_free()  # player falls
 	$Timer.start()
 
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
